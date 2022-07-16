8022d43769949659afb77044a69853e0f18d4017
Denis <gabbas.denn@mail.ru>
2022-07-16 08:50:40 +0300

    second experiment

--- script.js
+++ script.js
@@ -5,14 +5,20 @@ const btnPhone = document.querySelector('#iphone'),
       images = document.querySelectorAll('img');
 
 let phoneAnimation;
+let macbookAnimation;
+
 
 btnPhone.addEventListener('click', () => {
     if (!phoneAnimation) {
         phoneAnimation = images[0].animate([
-            {transform: 'translateY(0)'},
-            {transform: 'translateY(100px)'},
-            {transform: 'translateY(-100px)'},
-            {transform: 'translateY(0)'}
+            {transform: 'translateY(0) rotate(0deg)',
+            filter: 'opacity(100%)'},
+            {transform: 'translateY(100px) rotate(180deg)',
+            filter: 'opacity(50%)'},
+            {transform: 'translateY(-100px) rotate(270deg)',
+            filter: 'opacity(75%)'},
+            {transform: 'translateY(0) rotate(360deg)',
+            filter: 'opacity(100%)'}
         ], {
             duration: 3000,
             iterations: Infinity
@@ -21,5 +27,28 @@ btnPhone.addEventListener('click', () => {
         phoneAnimation.play();
     } else {
         phoneAnimation.pause();
+    }
+});
+
+
+btnMacbook.addEventListener('click', () => {
+    if (!macbookAnimation) {
+        macbookAnimation = images[1].animate([
+            {transform: 'translateX(0) rotate(0deg) scale(1)',
+            filter: 'opacity(100%)'},
+            {transform: 'translateX(100px) rotate(180deg) scale(2)',
+            filter: 'opacity(50%)'},
+            {transform: 'translateX(-100px) rotate(270deg) scale(3)',
+            filter: 'opacity(75%)'},
+            {transform: 'translateX(0) rotate(360deg) scale(2)',
+            filter: 'opacity(100%)'}
+        ], {
+            duration: 3000,
+            iterations: Infinity
+        });
+    } else if (macbookAnimation.playState === 'paused') {
+        macbookAnimation.play();
+    } else {
+        macbookAnimation.pause();
     }
 });
